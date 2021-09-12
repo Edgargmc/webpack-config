f7c199fd362250274765044deec1171bf0ea2795
Edgar <edgargmc@gmail.com>
2021-09-12 08:25:43 -0300

    Add: Babel

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # webpack-config
+
+https://www.youtube.com/watch?v=TOb1c39m64A
+https://youtu.be/TOb1c39m64A?t=854
+
+
+Windows
+/* set NODE_ENV=production& webpack */
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const elvenShieldRecipe = {
     leatherStrips:2,
     ironIngot:1,
-    refinedMoonstone: 4,
+    refinedMoonstone: 8,
+}
+
+const elvenGauntLetsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4
 }
 
 console.log(elvenShieldRecipe)
+console.log(elvenGauntLetsRecipe)
