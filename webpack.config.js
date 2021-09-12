27969aa002c0db27c68684a3848741396b8fbdda
Edgar <edgargmc@gmail.com>
2021-09-12 17:04:28 -0300

    add: React Fast Refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 const path = require("path")
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 
 let mode = "development"
 let target = "web"
+const plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    }),
+]
 
 if (process.env.NODE_ENV === "production") {
     mode = "production";
     target = "browserslist"
+} else {
+    plugins.push(new ReactRefreshWebpackPlugin())
 }
 
 module.exports = {
     mode: mode,
     target: target,
+    entry: "./src/index.js",
 
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -34,7 +45,7 @@ module.exports = {
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
-                        options: {publicPath: "" }
+                        options: {publicPath: ""}
                     },
                     "css-loader",
                     "postcss-loader",
@@ -53,13 +64,7 @@ module.exports = {
         ]
     },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
+    plugins: plugins,
 
     resolve: {
         extensions: [".js", ".jsx"]
