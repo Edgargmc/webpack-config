1bcb6882f189dbea0a1fcaca92abf6db6c0f6e94
Edgar <edgargmc@gmail.com>
2021-09-12 14:44:15 -0300

    add: images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png"
+import swordSvg from "../images/sword.svg"
 
  const App = () => {
      return (
          <>
-            <section className="heo"></section>
+            <section className="hero"></section>
              <main>
                  <section>
                      <h1>React Pa</h1>
                  </section>
              </main>
+             <img src={sword} alt="sword" width="250"/>
+             <img src={swordSvg} alt="swordSvg" width="250"/>
              <Recipes/>
          </>
      )
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
     mode: mode,
     target: target,
 
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+
     module: {
         rules: [
             {
@@ -22,7 +26,24 @@ module.exports = {
             },
             {
                 test: /\.(s[ac]|c)ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: "" }
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset", //resource, inline
+                // parser: {
+                //     dataUrlCondition: {
+                //         maxSize: 30 * 1024
+                //     }
+                // }
             }
         ]
     },
